11eeac231dcb135813b699ed9d9945faf257d0a3
FAYシ <phasordev@gmail.com>
2024-04-27 15:53:09 +0700

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+    logo: <span>Docs</span>,
+    darkMode: true, 
+    docsRepositoryBase: "https://github.com/FAYStarNext/docs.faystarnext.studio",
+    notFound: {
+        content: 'This page could not be found.',
+        labels: '404',
+    },
+    editLink: {
+        text: 'Edit this page on GitHub',
+    },
+    project: {
+        link: "https://github.com/FAYStarNext/bluelight.js",
+    },
+    footer: {
+        text: 'Docs',
+    },
+}
+
+export default config
